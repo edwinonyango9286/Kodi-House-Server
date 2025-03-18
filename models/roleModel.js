a1e2d5163c0205a443762fddc93fab7334f586c8
edwin onyango <ookoedwin29@gmail.com>
2025-03-18 11:02:55 +0300

    feat|| major changes in RBAC

--- models/roleModel.js
+++ models/roleModel.js
@@ -2,9 +2,20 @@ const mongoose = require("mongoose");
 
 const roleSchema = new mongoose.Schema(
   {
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      validate: {
+        validator: (id) => {
+          return mongoose.Types.ObjectId.isValid(id);
+        },
+        message: (props) => `${props} is not a valid object id.`,
+      },
+    },
     name: {
       type: String,
       required: true,
+      trim: true,
     },
     description: {
       type: String,
@@ -12,10 +23,16 @@ const roleSchema = new mongoose.Schema(
     },
     permissions: [
       {
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Permission",
+        validate: {
+          validator: (id) => {
+            return mongoose.Types.ObjectId.isValid(id);
+          },
+          message: (props) => `${props} is not a valid object id `,
+        },
       },
     ],
-
     isDeleted: {
       type: Boolean,
       default: false,
--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const {addARole,updateARole,getAllRoles,getARole,deleteARole} = require("../controllers/roleController");
+const { verifyUserToken, checkUserRole } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post("/add_role", addARole);
-router.get("/get_a_role/:roleId",getARole);
-router.put("/update_a_role/:roleId",updateARole);
-router.get("/get_all_roles",getAllRoles);
-router.delete("/delete_role/:roleId",deleteARole);
+
+router.post("/add_role", verifyUserToken, checkUserRole(["Admin","Landlord"]) , addARole);
+router.get("/get_a_role/:roleId", verifyUserToken,checkUserRole(["Admin","Landlord"]), getARole);
+router.put("/update_a_role/:roleId", verifyUserToken, checkUserRole(["Admin","Landlord"]), updateARole);
+router.get("/get_all_roles",verifyUserToken, checkUserRole(["Admin","Landlord"]), getAllRoles);
+router.delete("/delete_role/:roleId", verifyUserToken, checkUserRole(["Admin","Landlord"]), deleteARole);
 
 module.exports = router;
